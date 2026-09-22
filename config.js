2f0e036a439a13a961385817f3d896711bc2baa1
PRF <morofhamoi@gmail.com>
2026-09-22 17:34:43 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+window.VEXA_CONFIG = {
+  SUPABASE_URL: 'https://ypvqilhgpcnwabitqcib.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwdnFpbGhncGNud2FiaXRxY2liIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwODQ3MjAsImV4cCI6MjEwNTY2MDcyMH0.c-ZAK5K1tUW5NV0TikWZk4prCnxFEsRaMFod4pckJAI',
+  STORAGE_BUCKET: 'product-images',
+  APP_NAME: 'VEXA'
+};
